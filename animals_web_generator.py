a949313bf320eef89a4245ecae4161b8b2d9366f
Mikel <hallo.chupi@gmail.com>
2026-03-02 12:48:59 +0100

    Milestone 3: show error page when no animals are found

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -73,11 +73,47 @@ def generate_html(animals_data, query_name):
     new_html = template.replace("__REPLACE_ANIMALS_INFO__", animals_html)
     OUTPUT_FILE.write_text(new_html, encoding="utf-8")
 
+def generate_error_html(animal_name):
+    html = f"""<html>
+    <head>
+        <style>
+        html {{
+          background-color: #ffe9e9;
+        }}
+        body {{
+          font-family: 'Roboto','Helvetica Neue', Helvetica, Arial, sans-serif;
+          width: 900px;
+          margin: auto;
+          padding: 1rem;
+        }}
+        h1 {{
+          text-align: center;
+          font-size: 40pt;
+          font-weight: normal;
+        }}
+        h2 {{
+          text-align: center;
+        }}
+        </style>
+    </head>
+    <body>
+        <h1>My Animal Repository</h1>
+        <h2>The animal "{animal_name}" doesn't exist.</h2>
+    </body>
+</html>"""
+    OUTPUT_FILE.write_text(html, encoding="utf-8")
+
+
 
 def main():
     animal_name = input("Enter a name of an animal: ")
     animals = fetch_animals(animal_name)
-    generate_html(animals, animal_name)
+
+    if not animals:  # API returned an empty list
+            generate_error_html(animal_name)
+    else:
+            generate_html(animals, animal_name)
+
     print("Website was successfully generated to the file animals.html.")
 
 
